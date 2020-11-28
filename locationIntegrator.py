2d261febb6556c2822d81b8abf1aa135298b1db4
twesigye10 <twesigye10@gmail.com>
2020-11-28 17:11:12 +0300

    modified locationIntegrator

--- locationIntegrator.py
+++ locationIntegrator.py
@@ -69,30 +69,30 @@ feilds_to_add_flist = [
     ]
 
 # Sensor Location data
-stnLoc = [['004', 'Plaza de España', 439577.503, 4475070.366, 637, 'Urbana de tráfico'],
-    ['008', 'Escuelas Aguirre', 442117.68, 4474786.082, 672, 'Urbana de tráfico'],
-    ['011', 'Ramón y Cajal', 442567.611, 4478088.964, 708, 'Urbana de tráfico'],
-    ['016', 'Arturo Soria', 445788.017, 4476804.759, 698, 'Urbana de fondo'],
-    ['017', 'Villaverde', 439420.566, 4466527.998, 601, 'Urbana de fondo'],
-    ['018', 'Farolillo', 437893.845, 4471845.515, 581, 'Urbana de fondo'],
-    ['024', 'Casa de Campo', 436601.241, 4474586.797, 645, 'Suburbana'],
-    ['027', 'Barajas Pueblo', 450835.949, 4480855.369, 631, 'Urbana de fondo'],
-    ['035', 'Plaza del Carmen', 440344.765, 4474535.745, 657, 'Urbana de fondo'],
-    ['036', 'Moratalaz', 445254.758, 4473251.64, 671, 'Urbana de tráfico'],
-    ['038', 'Cuatro Caminos', 440036.799, 4477465.145, 699, 'Urbana de tráfico'],
-    ['039', 'Barrio del Pilar', 439693.285, 4481085.951, 673, 'Urbana de tráfico'],
-    ['040', 'Vallecas', 444705.01, 4471040.179, 677, 'Urbana de fondo'],
-    ['047', 'Méndez Álvaro', 441734.865, 4472167.017, 609, 'Urbana de fondo'],
-    ['048', 'Castellana', 441455.003, 4476789.943, 676, 'Urbana de tráfico'],
-    ['049', 'Parque del Retiro', 442095.972, 4473986.767, 662, 'Urbana de fondo'],
-    ['050', 'Plaza Castilla', 441622.45, 4479674.926, 728, 'Urbana de tráfico'],
-    ['054', 'Ensanche de Vallecas', 448055.403, 4469329.231, 630, 'Urbana de fondo'],
-    ['055', 'Urbanización Embajada', 450784.046, 4479256.762, 619, 'Urbana de fondo'],
-    ['056', 'Plaza Fernández Ladreda', 439003.502, 4470704.937, 605, 'Urbana de tráfico'],
-    ['057', 'Sanchinarro', 444026.471, 4482834.185, 700, 'Urbana de fondo'],
-    ['058', 'El Pardo', 434382.749, 4485555.056, 616, 'Suburbana'],
-    ['059', 'Juan Carlos I', 448379.789, 4479557.516, 669, 'Suburbana'],
-    ['060', 'Tres Olivos', 441559.491, 4483537.375, 715, 'Urbana de fondo']]
+stn_location_data = [['004', 'Plaza de España', 439577.503, 4475070.366, 637, 'Urbana de tráfico'],
+                     ['008', 'Escuelas Aguirre', 442117.68, 4474786.082, 672, 'Urbana de tráfico'],
+                     ['011', 'Ramón y Cajal', 442567.611, 4478088.964, 708, 'Urbana de tráfico'],
+                     ['016', 'Arturo Soria', 445788.017, 4476804.759, 698, 'Urbana de fondo'],
+                     ['017', 'Villaverde', 439420.566, 4466527.998, 601, 'Urbana de fondo'],
+                     ['018', 'Farolillo', 437893.845, 4471845.515, 581, 'Urbana de fondo'],
+                     ['024', 'Casa de Campo', 436601.241, 4474586.797, 645, 'Suburbana'],
+                     ['027', 'Barajas Pueblo', 450835.949, 4480855.369, 631, 'Urbana de fondo'],
+                     ['035', 'Plaza del Carmen', 440344.765, 4474535.745, 657, 'Urbana de fondo'],
+                     ['036', 'Moratalaz', 445254.758, 4473251.64, 671, 'Urbana de tráfico'],
+                     ['038', 'Cuatro Caminos', 440036.799, 4477465.145, 699, 'Urbana de tráfico'],
+                     ['039', 'Barrio del Pilar', 439693.285, 4481085.951, 673, 'Urbana de tráfico'],
+                     ['040', 'Vallecas', 444705.01, 4471040.179, 677, 'Urbana de fondo'],
+                     ['047', 'Méndez Álvaro', 441734.865, 4472167.017, 609, 'Urbana de fondo'],
+                     ['048', 'Castellana', 441455.003, 4476789.943, 676, 'Urbana de tráfico'],
+                     ['049', 'Parque del Retiro', 442095.972, 4473986.767, 662, 'Urbana de fondo'],
+                     ['050', 'Plaza Castilla', 441622.45, 4479674.926, 728, 'Urbana de tráfico'],
+                     ['054', 'Ensanche de Vallecas', 448055.403, 4469329.231, 630, 'Urbana de fondo'],
+                     ['055', 'Urbanización Embajada', 450784.046, 4479256.762, 619, 'Urbana de fondo'],
+                     ['056', 'Plaza Fernández Ladreda', 439003.502, 4470704.937, 605, 'Urbana de tráfico'],
+                     ['057', 'Sanchinarro', 444026.471, 4482834.185, 700, 'Urbana de fondo'],
+                     ['058', 'El Pardo', 434382.749, 4485555.056, 616, 'Suburbana'],
+                     ['059', 'Juan Carlos I', 448379.789, 4479557.516, 669, 'Suburbana'],
+                     ['060', 'Tres Olivos', 441559.491, 4483537.375, 715, 'Urbana de fondo']]
 
 # # ------------------Processing------------------
 arcpy.AddMessage("""
@@ -101,27 +101,26 @@ arcpy.AddMessage("""
 **************************************************
 """)
 
-# Create empty point Featureclass
+# Create empty point feature class(fc)
 create_fc_from_list(gdbLocation, out_name, geometry_type, has_m, has_z, sr)
-empData = out_name
+empty_fc = out_name
 
 # adding required fields
 for f_data in feilds_to_add_flist:
-    create_field_from_list(empData, f_data)
+    create_field_from_list(empty_fc, f_data)
 
 # Fetching Data from the madrid city council URL
 source = "http://www.mambiente.munimadrid.es/opendata/horario.txt"
 # store the decoded data in a 2D list
 raw_data = dataAggregator.prepare_raw_data(source)
-
 pldata = dataAggregator.aggregator_stn_data(raw_data)
-dWI = madpolindexcalculation.dataIndices(pldata)
+stn_with_index = madpolindexcalculation.dataIndices(pldata)
 
 # insert the data into the feature class
 #  Adding points and updating the fields
 fields = ["SHAPE@XY", "stnID", "stnName", "stnType", "stnElev", "MLAQI"]
-data_to_insert = formatted_stn_data_with_mlaqi(stnLoc, dWI)
-with arcpy.da.InsertCursor(empData, fields) as updLyr:
+data_to_insert = formatted_stn_data_with_mlaqi(stn_location_data, stn_with_index)
+with arcpy.da.InsertCursor(empty_fc, fields) as updLyr:
         for row in data_to_insert:
             updLyr.insertRow(row)
 
